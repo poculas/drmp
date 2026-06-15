7a3d5299c4e3aded3160e10a10459642ee4b5e98
chellie <poculas.nna@gmail.com>
2026-06-15 12:39:49 +0800

    Fix ImportError - remove non-existent user_signed_up signal

--- bakery/signals.py
+++ bakery/signals.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.signals import user_logged_in, user_logged_out, user_signed_up
+from django.contrib.auth.signals import user_logged_in, user_logged_out
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
